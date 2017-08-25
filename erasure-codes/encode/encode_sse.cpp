5260a1959277601520ba9c91c34b1bb48c962575
Phantomical <phantom@lynches.ca>
2017-08-25 13:30:03 -0400

    Made matrix_mul_sse and matrix_mul_avx2 more modular

--- erasure-codes/encode/encode_sse.cpp
+++ erasure-codes/encode/encode_sse.cpp
@@ -4,6 +4,75 @@
 
 namespace erasure
 {
+#ifdef _MSC_VER
+#	define restrict __restrict
+#endif
+
+	namespace 
+	{
+		void mul_add_row(
+			uint8_t val, 
+			const uint8_t* restrict in, 
+			uint8_t* restrict out, 
+			size_t num_bytes)
+		{
+			const __m128i mask = _mm_set1_epi8(0x0F);
+
+			__m128i lo = _mm_load_si128((const __m128i*)lohi_table[val][0]);
+			__m128i hi = _mm_load_si128((const __m128i*)lohi_table[val][1]);
+
+			for (size_t i = 0; i < num_bytes; i += sizeof(__m128i))
+			{
+				// in_val = in[i]
+				__m128i in_vals = _mm_load_si128((const __m128i*)(in + i));
+				// idx1 = in_val & 0xF
+				__m128i idx1 = _mm_and_si128(in_vals, mask);
+				// idx2 = (in_val >> 4) & 0xF
+				__m128i shifted = _mm_srli_epi64(in_vals, 4);
+				__m128i idx2 = _mm_and_si128(shifted, mask);
+				// lo_val = lo[idx1]
+				__m128i lo_vals = _mm_shuffle_epi8(lo, idx1);
+				// hi_val = hi[idx2]
+				__m128i hi_vals = _mm_shuffle_epi8(hi, idx2);
+				// result = lo_val ^ hi_val
+				__m128i result = _mm_xor_si128(lo_vals, hi_vals);
+				// out[i] ^= result
+				result = _mm_xor_si128(result, _mm_load_si128((__m128i*)(out + i)));
+				_mm_store_si128((__m128i*)(out + i), result);
+			}
+		}
+		void mul_row(
+			uint8_t val, 
+			const uint8_t* restrict in, 
+			uint8_t* restrict out,
+			size_t num_bytes)
+		{
+			const __m128i mask = _mm_set1_epi8(0x0F);
+
+			__m128i lo = _mm_load_si128((const __m128i*)lohi_table[val][0]);
+			__m128i hi = _mm_load_si128((const __m128i*)lohi_table[val][1]);
+
+			for (size_t i = 0; i < num_bytes; i += sizeof(__m128i))
+			{
+				// in_val = in[i]
+				__m128i in_vals = _mm_load_si128((const __m128i*)(in + i));
+				// idx1 = in_val & 0xF
+				__m128i idx1 = _mm_and_si128(in_vals, mask);
+				// idx2 = (in_val >> 4) & 0xF
+				__m128i shifted = _mm_srli_epi64(in_vals, 4);
+				__m128i idx2 = _mm_and_si128(shifted, mask);
+				// lo_val = lo[idx1]
+				__m128i lo_vals = _mm_shuffle_epi8(lo, idx1);
+				// hi_val = hi[idx2]
+				__m128i hi_vals = _mm_shuffle_epi8(hi, idx2);
+				// result = lo_val ^ hi_val
+				__m128i result = _mm_xor_si128(lo_vals, hi_vals);
+				// out[i] = result
+				_mm_store_si128((__m128i*)(out + i), result);
+			}
+		}
+	}
+
 	void matrix_mul_sse(
 		const matrix& mat,
 		const uint8_t* const* inputs,
@@ -13,12 +82,7 @@ namespace erasure
 		size_t num_bytes)
 	{
 		assert(num_bytes % 16 == 0);
-
-		for (size_t i = 0; i < n_outputs; ++i)
-			std::memset(outputs[i], 0, num_bytes);
 		
-		const __m128i mask = _mm_set1_epi8(0x0F);
-
 		for (size_t r = 0; r < n_outputs; ++r)
 		{
 			// Must be 16-byte aligned
@@ -29,28 +93,11 @@ namespace erasure
 				// Must be 16-byte aligned
 				const uint8_t* in = inputs[c];
 				const uint8_t val = mat(r, c).value;
-				__m128i lo = _mm_load_si128((const __m128i*)lohi_table[val][0]);
-				__m128i hi = _mm_load_si128((const __m128i*)lohi_table[val][1]);
-				
-				for (size_t i = 0; i < num_bytes; i += sizeof(__m128i))
-				{
-					// in_val = in[i]
-					__m128i in_vals = _mm_load_si128((const __m128i*)(in + i));
-					// idx1 = in_val & 0xF
-					__m128i idx1 = _mm_and_si128(in_vals, mask);
-					// idx2 = (in_val >> 4) & 0xF
-					__m128i shifted = _mm_srli_epi64(in_vals, 4);
-					__m128i idx2 = _mm_and_si128(shifted, mask);
-					// lo_val = lo[idx1]
-					__m128i lo_vals = _mm_shuffle_epi8(lo, idx1);
-					// hi_val = hi[idx2]
-					__m128i hi_vals = _mm_shuffle_epi8(hi, idx2);
-					// result = lo_val ^ hi_val
-					__m128i result = _mm_xor_si128(lo_vals, hi_vals);
-					// out[i] ^= result
-					result = _mm_xor_si128(result, _mm_load_si128((__m128i*)(out + i)));
-					_mm_store_si128((__m128i*)(out + i), result);
-				}
+
+				if (c == 0)
+					mul_row(val, in, out, num_bytes);
+				else
+					mul_add_row(val, in, out, num_bytes);
 			}
 		}
 	}
