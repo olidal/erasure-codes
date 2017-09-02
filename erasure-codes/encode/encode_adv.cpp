8c8a335f550c800b1e89a9cc847adbd3d04db88f
Phantomical <phantom@lynches.ca>
2017-09-02 11:42:42 -0400

    Migrated encoders to use row multiply methods from gfarithmetic

--- erasure-codes/encode/encode_adv.cpp
+++ erasure-codes/encode/encode_adv.cpp
@@ -2,42 +2,6 @@
 
 namespace erasure
 {
-	namespace adv
-	{
-		void mul_add_row(uint8_t val, const uint8_t* in, uint8_t* out, size_t num_bytes)
-		{
-			const uint8_t* lo = lohi_table[val][0];
-			const uint8_t* hi = lohi_table[val][1];
-
-			for (size_t i = 0; i < num_bytes; ++i)
-			{
-				uint8_t in_val = in[i];
-				uint8_t idx1 = in_val & 0xF;
-				uint8_t idx2 = in_val >> 4;
-				uint8_t lo_val = lo[idx1];
-				uint8_t hi_val = hi[idx2];
-				uint8_t result = lo_val ^ hi_val;
-				out[i] ^= result;
-			}
-		}
-		void mul_row(uint8_t val, const uint8_t* in, uint8_t* out, size_t num_bytes)
-		{
-			const uint8_t* lo = lohi_table[val][0];
-			const uint8_t* hi = lohi_table[val][1];
-
-			for (size_t i = 0; i < num_bytes; ++i)
-			{
-				uint8_t in_val = in[i];
-				uint8_t idx1 = in_val & 0xF;
-				uint8_t idx2 = in_val >> 4;
-				uint8_t lo_val = lo[idx1];
-				uint8_t hi_val = hi[idx2];
-				uint8_t result = lo_val ^ hi_val;
-				out[i] = result;
-			}
-		}
-	}
-
 	void matrix_mul_adv(
 		const matrix& mat,
 		const uint8_t* const* inputs,
